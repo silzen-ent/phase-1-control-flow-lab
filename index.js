41539c08304239735a939966f2efe55b032df173
Alan Silva <silzen.ent@gmail.com>
2023-12-13 20:06:07 -0700

    completed phase 1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if (feet <= 400){
+    return "This one is on me!";
+  }
+  else if (feet <= 2000){
+    return "That will be twenty bucks."
+  } 
+  else if (2000 < feet && feet <= 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do.";
+  } 
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you.";
+      default:
+        return 'Bye.'
+  }
 }
